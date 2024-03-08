9653055ebf397d5aebee30378c8edd4bbbff8314
abhinav3254 <abhinav3254@gmail.com>
2024-03-08 15:41:54 +0530

    add node done

--- src/Components/Flow.jsx
+++ src/Components/Flow.jsx
@@ -16,13 +16,13 @@ import './Flow.scss'
 import EdgeDialogBox from './EdgeDialogBox';
 
 const initialNodes = [
-    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-    { id: '3', position: { x: 200, y: 200 }, data: { label: '3' } },
+    // { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    // { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+    // { id: '3', position: { x: 200, y: 200 }, data: { label: '3' } },
 ];
 const initialEdges = [
-    { id: 'e1-3', source: '1', target: '3' },
-    { id: 'e2-3', source: '2', target: '3' },
+    // { id: 'e1-3', source: '1', target: '3' },
+    // { id: 'e2-3', source: '2', target: '3' },
 ];
 
 function Flow() {
